c49af0f78861af3810f44e25551f19008e313f16
Daniel Chae <daniel.jiwoong@gmail.com>
2025-07-16 13:58:32 +0900

    Add page metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from "next";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/AppSidebar";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Kubrick",
+  description: "Kubrick Semantic Video Search Playground",
+};
 
 const RootLayout = ({
   children,
