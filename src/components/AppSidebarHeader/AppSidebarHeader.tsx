2530b74e7b0be3a24102e11acc2fd2a660ac370d
Dominique Morales Perez <dmp@Dominiques-MacBook-Pro.local>
2025-07-15 18:25:09 -0700

    feat: Added navigation sidebar

---
+++ src/components/AppSidebarHeader/AppSidebarHeader.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const AppSidebarHeader = () => {
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <SidebarMenuButton
+          size="lg"
+          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+        >
+          <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg"></div>
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-medium">Kubrick</span>
+            <span className="truncate text-xs">Placeholder text</span>
+          </div>
+        </SidebarMenuButton>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+};
+
+export default AppSidebarHeader;
